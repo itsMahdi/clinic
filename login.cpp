9e0de697254787c9c78e092cc10bb50c05bf335b
itsMahdi <mahdipourghasem@gmail.com>
2018-01-10 21:03:18 +0330

    first commit!

---
+++ login.cpp
@@ -0,0 +1,14 @@
+#include "login.h"
+#include "ui_login.h"
+
+LogIn::LogIn(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::LogIn)
+{
+    ui->setupUi(this);
+}
+
+LogIn::~LogIn()
+{
+    delete ui;
+}
---
+++ login.h
@@ -0,0 +1,22 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class LogIn;
+}
+
+class LogIn : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit LogIn(QWidget *parent = 0);
+    ~LogIn();
+
+private:
+    Ui::LogIn *ui;
+};
+
+#endif // LOGIN_H
